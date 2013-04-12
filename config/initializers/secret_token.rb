ae6e07bc1addd458a3abe35636fcb1e6d2e5c1f4
mryoshio <ashitaba.yoshioka@leonisand.co>
2013-04-13 01:54:17 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nagare::Application.config.secret_token = '137a229142fed79e8c9fc276cde6d8046e66f87374641a8e819ad53921d98920baa2667c7c7a070777f16d34dd3eec3467b24b77fd8126adf3e395dbfdad3083'
