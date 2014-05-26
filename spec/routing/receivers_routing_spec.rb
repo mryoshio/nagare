8ebc8e5d6e9c1fb154f0dcd807dbc42f15dd5993
mryoshio <ashitaba.yoshioka@leonisand.co>
2014-05-26 12:34:31 +0900

    update routing spec

--- spec/routing/receivers_routing_spec.rb
+++ spec/routing/receivers_routing_spec.rb
@@ -1,35 +1,15 @@
 require "spec_helper"
 
 describe ReceiversController do
+
   describe "routing" do
-
-    it "routes to #index" do
-      get("/receivers").should route_to("receivers#index")
-    end
-
-    it "routes to #new" do
-      get("/receivers/new").should route_to("receivers#new")
-    end
-
-    it "routes to #show" do
-      get("/receivers/1").should route_to("receivers#show", :id => "1")
-    end
-
-    it "routes to #edit" do
-      get("/receivers/1/edit").should route_to("receivers#edit", :id => "1")
-    end
-
-    it "routes to #create" do
-      post("/receivers").should route_to("receivers#create")
-    end
-
-    it "routes to #update" do
-      put("/receivers/1").should route_to("receivers#update", :id => "1")
-    end
-
-    it "routes to #destroy" do
-      delete("/receivers/1").should route_to("receivers#destroy", :id => "1")
-    end
-
+    it { expect(get: "/receivers").to route_to(controller: "receivers", action: "index") }
+    it { expect(get: "/receivers/new").to route_to(controller: "receivers", action: "new") }
+    it { expect(get: "/receivers/1").to route_to(controller: "receivers", action: "show", id: "1") }
+    it { expect(get: "/receivers/1/edit").to route_to(controller: "receivers", action: "edit", id: "1") }
+    it { expect(post: "/receivers").to route_to(controller: "receivers", action: "create") }
+    it { expect(put: "/receivers/1").to route_to(controller: "receivers", action: "update", id: "1") }
+    it { expect(delete: "/receivers/1").to route_to(controller: "receivers", action: "destroy", id: "1") }
   end
+
 end
