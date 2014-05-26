fbe400e3048140f36bd2f8fcd0b4ca655264237b
mryoshio <ashitaba.yoshioka@leonisand.co>
2014-05-26 20:49:21 +0900

    update beacon_holder views

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,6 @@
 module DashboardHelper
+  def beacon_holder_is_creatable?
+    return true if !Beacon.count.zero? && !Patient.count.zero?
+    false
+  end
 end
--- app/models/beacon_holder.rb
+++ app/models/beacon_holder.rb
@@ -1,2 +1,4 @@
 class BeaconHolder < ActiveRecord::Base
+  belongs_to :beacon
+  belongs_to :patient
 end
