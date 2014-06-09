57588330d3fe712f3e4553c10ab7f17afffbfbb2
mryoshio <yoshiokaas@gmail.com>
2014-06-09 22:52:56 +0900

    remove view specs

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,16 @@ module Nagare
 
     # Redis
     ENV["REDISTOGO_URL"] ||= "redis://localhost:6379"
+
+    config.generators do |g|
+      g.test_framework :rspec,
+      fixtures: true,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: true,
+      controller_specs: true,
+      request_specs: false
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
   end
 end
