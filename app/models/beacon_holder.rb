2a21facf5f94efb2a5592dd54aea5b731a8129c2
mryoshio <yoshiokaas@gmail.com>
2014-07-23 01:56:23 +0900

    specify namespace in place model

--- app/models/beacon_holder.rb
+++ app/models/beacon_holder.rb
@@ -3,6 +3,6 @@ class BeaconHolder < ActiveRecord::Base
   belongs_to :patient
 
   def self.creatable?
-    (Beacon.count.zero? || Patient.count.zero?) ? false : true
+    (::Beacon.count.zero? || ::Patient.count.zero?) ? false : true
   end
 end
